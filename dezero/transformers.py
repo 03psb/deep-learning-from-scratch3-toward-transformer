8b92a4c81e8879d29bc97c80a5f378baea666ef0
03psb <98043513+03psb@users.noreply.github.com>
2025-09-26 20:05:37 +0900

    Update transformers.py
    Finished SingleHeadAttention. Should work on MultiHeadAttention

--- dezero/transformers.py
+++ dezero/transformers.py
@@ -17,13 +17,14 @@ class SingleHeadAttention(Layer):
         self.d_v = d_v
         self.dtype = dtype
 
-        self.w_q = Linear(d_k, in_size=d_model)
-        self.w_k = Linear(d_k, in_size=d_model)
-        self.w_v = Linear(d_v, in_size=d_model)
-
-    def forward(self, x):
-        q = self.w_q(x)
-        k = self.w_k(x)
-        v = self.w_v(x)
-        attention = F.softmax(F.matmul(q, k.T) / )
+        self.W_Q = Linear(d_k, in_size=d_model)
+        self.W_K = Linear(d_k, in_size=d_model)
+        self.W_V = Linear(d_v, in_size=d_model)
         
+    def forward(self, x):
+        q = self.W_Q(x)
+        k = self.W_K(x)
+        v = self.W_V(x)
+        s_out = F.softmax(F.matmul(q, k.T) / np.sqrt(d_model))
+        y = F.matmul(s_out, v)
+        return y
