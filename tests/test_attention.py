fe72fb590a8ec4ab127bf7964c8b58865cc6d9a3
03psb <98043513+03psb@users.noreply.github.com>
2025-09-27 20:20:46 +0900

    Create test_attention.py
    I'm not sure this works...

---
+++ tests/test_attention.py
@@ -0,0 +1,14 @@
+import unittest
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+from dezero.utils import gradient_check, array_allclose
+
+
+class TestAttention(unittest.TestCase):
+
+    def test_forward1(self):
+        pass
+
+    def test_backward1(self):
+        pass
